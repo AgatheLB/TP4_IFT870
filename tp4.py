cbc8b0305313058abd2e45b0dd2a5b30bd185933
Agath <agathe.lebouler@gmail.com>
2020-04-01 23:17:39 +0200

    q3b cross validation with kmeans and ARI, plot mean test scores

--- tp4.py
+++ tp4.py
@@ -1,8 +1,6 @@
 # %%
 # leba3207
 
-from collections import defaultdict
-
 import sklearn
 import numpy as np
 import scipy as sp
@@ -14,6 +12,9 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn import metrics
 from scipy.spatial.distance import cdist
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import make_scorer
+from sklearn.metrics.cluster import adjusted_rand_score
 
 # %%
 #Chargement d'un ensemble de données de faces de personnages connus
@@ -62,14 +63,17 @@ data = pd.DataFrame(faces.data)
 
 filtered_faces = dict()
 filtered_data = pd.DataFrame()
+filtered_target = []
 for i, (tn, nb) in enumerate(zip(faces.target_names, number_target_per_face)):
     # retrieve first forty indexes of target associated
     positions = np.where(faces.target == i)[0][:40]
-
+    
     filtered_faces[tn] = []
     for p in positions:
         filtered_faces[tn].append(faces.images[p])
         filtered_data.loc[:, p] = data.iloc[p]
+        filtered_target.append(faces.target[p])
+
 
 filtered_data = filtered_data.T
 
@@ -86,19 +90,40 @@ reduced_data = pca.fit_transform(filtered_data)
 # %%
 """
 # 3. Analyse avec K-Means
-## a. méthode Elbow
+## a. Méthode Elbow
 """
 
 # %%
+K = range(40, 85, 5)
 mean_distances = []
-for k in range(40, 85, 5):
+for k in K:
     model = KMeans(n_clusters=k)
     model.fit(reduced_data)
     dist_to_best_centroid = np.min(cdist(reduced_data, model.cluster_centers_, 'euclidean'), axis=1)
     mean_distances.append(sum(dist_to_best_centroid) / reduced_data.shape[0])
 
-plt.plot(range(40, 85, 5), mean_distances, 'bx-')
+plt.plot(K, mean_distances, 'bx-')
 plt.xlabel('k')
 plt.ylabel('Mean distance')
 plt.title('Elbow Method with K-Means model')
+plt.show()
+
+# %%
+"""
+## b. Approche de validation croisée
+"""
+
+# %%
+
+mean_scores = []
+for k in K:
+    scorer = make_scorer(adjusted_rand_score)
+    model = KMeans(n_clusters=k)
+    cv_results = cross_validate(model, reduced_data, filtered_target, cv=10, scoring=scorer, n_jobs=-1)
+    mean_scores.append(np.mean(cv_results['test_score']))
+
+plt.plot(K, mean_scores, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Mean score')
+plt.title('Cross validation with K-Means model')
 plt.show()
